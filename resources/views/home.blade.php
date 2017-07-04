bac1799725486ee75f57d9219390bc92a213ad6c
Lerry <immanuelvalerie1@gmail.com>
2017-07-04 13:42:42 -0700

    Form Home (Pengajuan rekomendasi reklame)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,19 @@
 
 
     </div>
+    
+    <br>
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <a href="/assets/images/150.jpg">
+        <img src="/assets/images/150.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <p align="center">Pengajuan Rekomendasi Reklame</p>
+        </div>
+      </a>
+    </div>
+  </div>
+
+
 </div>
 @endsection
